d742a8d4f57a86bf5588e9a7de376fee3f818995
Plamen Todorov <peerless@Plamens-MacBook.local>
2013-11-20 16:19:17 +0200

    Initial Version

---
+++ XcodeKit/XcodeKit.h
@@ -0,0 +1,27 @@
+//
+//  XcodeKit.h
+//  XcodeKit
+//
+//  Created by Plamen Todorov on 20.11.13.
+//  Copyright (c) 2013 г. Plamen Todorov. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface XcodeKit : NSObject
+{
+    NSRange currentRange;
+    NSRange currentLineRange;
+    NSString *currentSelection;
+    
+    NSTextView *codeEditor;
+    
+}
+
+@property (assign) NSRange currentRange;
+@property (assign) NSRange currentLineRange;
+@property (nonatomic, retain) NSString *currentSelection;
+@property (nonatomic, retain) NSTextView *codeEditor;
+
+-(id)initWithBundle:(NSBundle *)plugin;
+@end
---
+++ XcodeKit/XcodeKit.m
@@ -0,0 +1,155 @@
+//
+//  XcodeKit.m
+//  XcodeKit
+//
+//  Created by Plamen Todorov on 20.11.13.
+//  Copyright (c) 2013 г. Plamen Todorov. All rights reserved.
+//
+
+#import "XcodeKit.h"
+
+static XcodeKit *sharedPlugin;
+
+@interface XcodeKit()
+@property (nonatomic, strong) NSBundle *bundle;
+@end
+
+@implementation XcodeKit
+@synthesize codeEditor, currentRange, currentLineRange, currentSelection;
+
++(void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    
+    if([currentApplicationName isEqual:@"Xcode"]){
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[self alloc] initWithBundle:plugin];
+        });
+    }
+}
+
+-(id)initWithBundle:(NSBundle *)plugin
+{
+    if(self = [super init])
+    {
+        self.bundle = plugin;
+     
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
+        
+        if(menuItem)
+        {
+            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+            
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Delete Selection / Line" action:@selector(deleteSelection) keyEquivalent:@""];
+            [actionMenuItem setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem];
+            
+            NSMenuItem *actionMenuItem2 = [[NSMenuItem alloc] initWithTitle:@"Duplicate Selection / Line" action:@selector(duplicateSelection) keyEquivalent:@""];
+            [actionMenuItem2 setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem2];
+            
+            // tap into notifications
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(selectionDidChange:) name:NSTextViewDidChangeSelectionNotification object:nil];
+            
+            if(!self.codeEditor){
+                NSResponder *firstResponder = [[NSApp keyWindow] firstResponder];
+                if([firstResponder isKindOfClass:NSClassFromString(@"DVTSourceTextView")] && [firstResponder isKindOfClass:[NSTextView class]]){
+                    self.codeEditor = (NSTextView *)firstResponder;
+                }
+            }
+            
+            if(self.codeEditor){
+                NSNotification *notification = [NSNotification notificationWithName:NSTextViewDidChangeSelectionNotification object:self.codeEditor];
+                [self selectionDidChange:notification];
+            }
+        }
+    }
+    
+    return self;
+}
+
+-(BOOL)validateMenuItem:(NSMenuItem *)menuItem
+{
+    if([menuItem action] == @selector(deleteSelection))
+    {
+        NSResponder *firstResponder = [[NSApp keyWindow] firstResponder];
+        return ([firstResponder isKindOfClass:NSClassFromString(@"DVTSourceTextView")] && [firstResponder isKindOfClass:[NSTextView class]]);
+    }
+    else if([menuItem action] == @selector(duplicateSelection))
+    {
+        NSResponder *firstResponder = [[NSApp keyWindow] firstResponder];
+        return ([firstResponder isKindOfClass:NSClassFromString(@"DVTSourceTextView")] && [firstResponder isKindOfClass:[NSTextView class]]);
+    }
+    
+    return YES;
+}
+
+-(void)selectionDidChange:(NSNotification *)notification
+{
+    if([[notification object] isKindOfClass:NSClassFromString(@"DVTSourceTextView")] && [[notification object] isKindOfClass:[NSTextView class]])
+    {
+		self.codeEditor = (NSTextView *)[notification object];
+        
+        NSArray *selectedRanges = [codeEditor selectedRanges];
+        
+        if(selectedRanges.count >= 1)
+        {
+			NSString *code = codeEditor.textStorage.string;
+			
+            self.currentRange     = [[selectedRanges objectAtIndex:0] rangeValue];
+            self.currentLineRange = [code lineRangeForRange:currentRange];
+            self.currentSelection = [code substringWithRange:currentRange];
+			
+            //NSRange rangeInLine = NSMakeRange(currentRange.location - currentLineRange.location, currentRange.length);
+            //NSLog(@"%@", NSStringFromRange(currentRange));
+        }
+    }
+}
+
+-(void)deleteSelection
+{
+    if(codeEditor)
+	{
+		if(currentSelection && [currentSelection isNotEqualTo:@""]){
+            [codeEditor insertText:@"" replacementRange:currentRange];
+		}
+        else {
+            
+            long start, end;
+            
+            start = currentLineRange.location - 1;
+            end = currentLineRange.length;
+            
+            [codeEditor insertText:@"" replacementRange:NSMakeRange(start, end)];
+        }
+	}
+}
+
+-(void)duplicateSelection
+{
+    if(codeEditor)
+	{
+		if(currentSelection && [currentSelection isNotEqualTo:@""])
+        {
+            NSString *copy = currentSelection;
+            
+            [codeEditor setSelectedRange:NSMakeRange(currentRange.location + currentRange.length, 0)];
+            [codeEditor insertText:copy];
+		}
+        else {
+            NSString *lineContent = [codeEditor.textStorage.string substringWithRange:currentLineRange];
+            
+            [codeEditor setSelectedRange:NSMakeRange(currentLineRange.location + currentLineRange.length, 0)];
+            [codeEditor insertText:lineContent];
+        }
+	}
+}
+
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:NSTextViewDidChangeSelectionNotification object:nil];
+}
+
+@end
